17d02dd40a1eaefaa2c66857faf1b55eece42eb8
Isaac M. Good <imgoodproductions@sbcglobal.net>
2024-07-19 17:42:14 -0700

    Stub out website crate

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,3 +1,14 @@
+use yew::{function_component, html, Html, Renderer};
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div>
+            <p>{ "Hello, verity solver!" }</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Renderer::<App>::new().render();
 }
