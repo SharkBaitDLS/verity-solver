896d36651ab041d36802a8dd5aea4d673967b2a6
Isaac M. Good <imgoodproductions@sbcglobal.net>
2024-07-21 01:35:44 -0700

    Mock up shape selection buttons

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,3 @@
-use solver::solve;
-use verity_solver_models::{CompositeShape, InsideStatues, OutsideStatues, Shape};
-
-mod solver;
-
 fn main() -> Result<(), String> {
-    let outside_state = OutsideStatues {
-        left: CompositeShape::new(Shape::Square, Shape::Triangle),
-        middle: CompositeShape::new(Shape::Triangle, Shape::Circle),
-        right: CompositeShape::new(Shape::Square, Shape::Circle),
-    };
-
-    let inside_state = InsideStatues {
-        left: Shape::Square,
-        middle: Shape::Circle,
-        right: Shape::Triangle,
-    };
-
-    solve(inside_state, outside_state)?;
     Ok(())
 }
--- website/src/main.rs
+++ website/src/main.rs
@@ -1,10 +1,34 @@
 use yew::{function_component, html, Html, Renderer};
 
+#[allow(dead_code)]
+mod solver;
+
 #[function_component]
 fn App() -> Html {
     html! {
-        <div>
-            <p>{ "Hello, verity solver!" }</p>
+        <div class="main is-flex is-flex-direction-column">
+            <main>
+                <p>{ "Hello, verity solver! Have some mocked-up buttons!" }</p>
+                <div class="hex-container">
+                    <div class="hex-border">
+                        <button class="hex-segment-active hex-segment-top"/>
+                    </div>
+                    <div class="hex-border">
+                        <button class="hex-segment-inactive hex-segment-left"/>
+                    </div>
+                    <div>
+                        <button class="hex-segment-clicked hex-segment-right"/>
+                    </div>
+                </div>
+            </main>
+            <footer class="footer mt-auto">
+                <div class="content has-text-centered">
+                    { "Powered by " }
+                    <a href="https://yew.rs">{ "Yew" }</a>
+                    { " using " }
+                    <a href="https://bulma.io">{ "Bulma" }</a>
+                </div>
+            </footer>
         </div>
     }
 }
