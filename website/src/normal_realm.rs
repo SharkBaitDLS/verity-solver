c6f6b8d568a0af8f3dd9d9bd6d09e4fcd004afb2
Isaac M. Good <imgoodproductions@sbcglobal.net>
2024-07-25 14:59:54 -0700

    Basic prototype layout for normal realm

--- website/src/normal_realm.rs
+++ website/src/normal_realm.rs
@@ -1,8 +1,25 @@
 use yew::{function_component, html, Html};
 
+use crate::shape_picker::ShapePicker;
+
 #[function_component]
 pub fn NormalRealm() -> Html {
     html! {
-        <p>{"This is the normal realm page"}</p>
+        <div>
+            <div class="level">
+                <div class="columns level-item">
+                    <ShapePicker multi_shape={false}/>
+                    <ShapePicker multi_shape={false}/>
+                    <ShapePicker multi_shape={false}/>
+                </div>
+            </div>
+            <div class="level">
+                <div class="columns level-item">
+                    <ShapePicker multi_shape={true}/>
+                    <ShapePicker multi_shape={true}/>
+                    <ShapePicker multi_shape={true}/>
+                </div>
+            </div>
+        </div>
     }
 }
